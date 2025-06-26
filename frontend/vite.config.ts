5c19e4224e0a69e528ccc249896d42c032dfe75d
Teerth Shailesh Patel <teerthshailesh.patel@scranton.edu>
2025-06-26 09:37:40 -0400

    deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()] ,
+  plugins: [react()],
   base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
+    assetsDir: 'assets'
   }
 })

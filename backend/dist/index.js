d880d93e685eefac14f30df2c4d00f33f5653bd9
Teerth Shailesh Patel <teerthshailesh.patel@scranton.edu>
2025-06-27 17:17:06 -0400

    final_backend_deployment

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -10,8 +10,30 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const port = 3000;
 const app = (0, express_1.default)();
+// const corsOptions = {
+//     origin: [
+//       'http://localhost:3000/api/v1', // Your local development frontend
+//       'https://lottery-tracker-five.vercel.app', // Your Vercel frontend
+//       'http://184.73.150.172:3000' // Your backend itself
+//     ],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+//   };
+const corsOptions = {
+    origin: [
+        'http://localhost:5173', // Vite default dev server
+        'http://localhost:3001', // Alternative local port
+        'http://localhost:3000', // Another common port
+        'https://lottery-tracker-five.vercel.app', // Your Vercel frontend
+        'https://transcript-lift-relationship-phd.trycloudflare.com',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)(corsOptions));
 app.use("/api/v1", index_1.default);
 app.listen(port, '0.0.0.0', () => {
     console.log(`Listening to port ${port}`);
--- backend/index.ts
+++ backend/index.ts
@@ -9,16 +9,29 @@ dotenv.config();
 const port = 3000;
 const app = express();
 
-const corsOptions = {
+// const corsOptions = {
+//     origin: [
+//       'http://localhost:3000/api/v1', // Your local development frontend
+//       'https://lottery-tracker-five.vercel.app', // Your Vercel frontend
+//       'http://184.73.150.172:3000' // Your backend itself
+//     ],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+//   };
+
+  const corsOptions = {
     origin: [
-      'http://localhost:5173', // Your local development frontend
+      'http://localhost:5173', // Vite default dev server
+      'http://localhost:3001', // Alternative local port
+      'http://localhost:3000', // Another common port
       'https://lottery-tracker-five.vercel.app', // Your Vercel frontend
-      'http://13.218.58.81:3000' // Your backend itself
+      'https://transcript-lift-relationship-phd.trycloudflare.com',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
-  };
+}
 
 app.use(express.json());
 app.use(cors(corsOptions))

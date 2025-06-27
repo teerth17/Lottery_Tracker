7e8a369904cc05fbfb6dafc72adc0c21b5d3e282
Teerth Shailesh Patel <teerthshailesh.patel@scranton.edu>
2025-06-27 10:02:33 -0400

    deplendeny_added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  },
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer
-      ]
-    }
-  }
+  plugins: [react(), tailwindcss()] 
 })
